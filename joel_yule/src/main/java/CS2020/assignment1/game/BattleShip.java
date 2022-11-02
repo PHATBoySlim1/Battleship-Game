3ccdb3c6088174804b854b618b529505dc21c5c4
JOEL YULE <j.yule.21@abdn.ac.uk>
2022-11-02 12:53:27 +0000

    improved functionality of battleship

--- joel_yule/src/main/java/CS2020/assignment1/game/BattleShip.java
+++ joel_yule/src/main/java/CS2020/assignment1/game/BattleShip.java
@@ -29,14 +29,14 @@ public class BattleShip extends AbstractBattleShip {
         this.shipCoordinates = coordinates;
     }
 
-    public String getName() {return name;}
-    public int getHits() {return hits;}
-    public String getShipOrientation(){return shipOrientation;}
-    public int[][] getShipCoordinates(){return shipCoordinates;}
+    public String getName() {return this.name;}
+    public int getHits() {return this.hits;}
+    public String getShipOrientation(){return this.shipOrientation;}
+    public int[][] getShipCoordinates(){return this.shipCoordinates;}
 
     //Method to check if the attack was a hit or not
     public boolean checkAttack(int row, int column){
-        if(hits == 3){
+        if(getHits() == 3){
             return false;
         }
         else {
